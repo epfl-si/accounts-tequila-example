8d4a15bc0d66b7e3403dc9e652e661adf80046f6
Nicolas Borboën <ponsfrilus@gmail.com>
2019-11-12 12:49:45 +0100

    [FT.] Login/Logout link withRouter

--- imports/ui/Header.jsx
+++ imports/ui/Header.jsx
@@ -1,26 +1,57 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link, withRouter} from 'react-router-dom';
+import { withTracker } from 'meteor/react-meteor-data';
 
-export default Header = () => {
-  return (
-    <header className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
-      <Link className="navbar-brand" to="/">Home</Link>
-      <div className="collapse navbar-collapse">
-        <ul className="navbar-nav mr-auto"> 
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Doc
-            </a>
-            <div className="dropdown-menu" >
-              <Link className="dropdown-item" to="/doc/README.md">Readme</Link>
-              <Link className="dropdown-item" to="/doc/TEQUILA_ATTRIBUTES.md">Tequila attributes</Link>
-            </div>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/private">Private</a>
-          </li>
-        </ul>
-      </div>
-    </header>
-  )
+class Header extends Component {
+  logMeOutNow = () => {
+    Meteor.logout()
+    this.props.history.push('/');
+  }
+  displayLoginLogout = () => {
+    if (this.props.userId) {
+      return <a className="logout-link" href="" onClick={this.logMeOutNow}>logout</a>
+    } else {
+      return <a className="login-link" href="/private">login</a>
+    }
+  }
+  render() {
+    return (
+      <header className="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <Link className="navbar-brand" to="/">Home</Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto"> 
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Doc
+              </a>
+              <div className="dropdown-menu" >
+                <Link className="dropdown-item" to="/doc/README.md">Readme</Link>
+                <Link className="dropdown-item" to="/doc/TEQUILA_ATTRIBUTES.md">Tequila attributes</Link>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/private">Private</a>
+            </li>
+          </ul>
+        </div>
+        <div className="userMenu">
+          { this.displayLoginLogout() }
+        </div>
+      </header>
+    )
+  }
 }
+
+
+
+export default withRouter(withTracker(() => {
+  return {  
+    userId: Meteor.userId(),
+  };  
+})(Header));
+
+
+
+
+
+
