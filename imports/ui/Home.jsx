1776fbad6a847c0c8d6dc9a18cbc75e6e0ec5322
Nicolas Borboën <ponsfrilus@gmail.com>
2020-02-19 14:42:20 +0100

    [DOC] Shaping a homepage

--- imports/ui/Home.jsx
+++ imports/ui/Home.jsx
@@ -5,7 +5,21 @@ export default class Home extends Component {
 
   render() {
     return (
-      <h1>Accounts Tequila example</h1>
+      <div className="main">
+        <h1>Accounts Tequila example</h1>
+        <p>
+          This is a <a href="https://www.meteor.com/" target="_blank">Meteor</a> app using the <a href="http://www.passportjs.org/" target="_blank">Passport authentication middleware</a> with the <a href="https://github.com/epfl-si/accounts-tequila" target="_blank">accounts-tequila</a> strategy.
+          The <a href="https://github.com/epfl-si/accounts-tequila" target="_blank">accounts-tequila</a> strategy uses the Meteor's core package <a href="https://guide.meteor.com/accounts.html#accounts-base" target="_blank">accounts-base</a>.
+          Consequently, the <a href="https://docs.meteor.com/api/accounts.html" target="_blank">documentation of how to use Meteor's accounts functionality</a> is resourceful.
+        </p>
+        <ul>
+          <li>Please check the <a href="/doc/README.md">README.md</a> of the project.</li>
+          <li>EPFL Tequila attributes are listed <a href="/doc/TEQUILA_ATTRIBUTES.md">here</a>.</li>
+          <li>You can try to login <a href="/private">here</a>.</li>
+        </ul>
+        <hr />
+        <p>Please join this project, question, issues and pull request are very welcome: <a href="https://github.com/epfl-si/accounts-tequila-example/issues">https://github.com/epfl-si/accounts-tequila-example/issues</a>.</p>
+      </div>
     )
   }
 }
