0e8aad06f5833731e2135c56492b42564f455125
Nicolas Borboën <ponsfrilus@gmail.com>
2020-02-26 14:37:21 +0100

    [WIP] Roles

--- server/main.js
+++ server/main.js
@@ -11,6 +11,9 @@ const debug = debug_('accounts-tequila-example');
 
 Meteor.startup(() => {
 
+  // Ensure that the default roles exists in the 'roles' collection
+  loadFixtures();
+
   // https://docs.meteor.com/api/accounts.html#Meteor-users
   // "By default, the current user’s username, emails and profile are published to the client.
   // You can publish additional fields for the current user with:"
@@ -39,7 +42,18 @@ Meteor.startup(() => {
     getUserId(tequila) {
       //debug(tequila);
       console.debug(tequila);
-
+      // All members of the group 'accounts-tequila-example' get a role
+      if (tequila.group.includes('accounts-tequila-example')) {
+        // Some of the members are granted admin role, based on their username
+        if (['charmier', 'nborboen'].includes(tequila.user)) {
+          Roles.setUserRoles(tequila.uniqueid, ['admin'], Roles.GLOBAL_GROUP);
+          console.debug(tequila.user, 'ADMIN ROLE');
+        // All others members are granted the editor role...
+        } else {
+          Roles.setUserRoles(tequila.uniqueid, ['editor'], Roles.GLOBAL_GROUP);
+          console.debug(tequila.user, 'EDITORS ROLE');
+        }
+      }
       return tequila.uniqueid;
     },
     upsert: (tequila) => ({ $set: {
